0c475e7a613cd25b7ed0f25eb3e86ff1bbee20c9
Aleksandr Sleptsov <a.sleptsov@dunice.net>
2022-09-22 12:50:12 +0300

    Fix the path to the built module

--- dist/esm/example/src/Sidebar.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import type { IHighlight } from "./react-pdf-highlighter";
-interface Props {
-    highlights: Array<IHighlight>;
-    resetHighlights: () => void;
-    toggleDocument: () => void;
-}
-export declare function Sidebar({ highlights, toggleDocument, resetHighlights, }: Props): JSX.Element;
-export {};
